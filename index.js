0962ab9349e1bce08909a6f1e8157702cd9f9906
Atul <atulnayak7869@gmail.com>
2023-07-30 14:26:00 +0530

    scroll commit

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const header=document.querySelector("header")
 window.addEventListener("scroll",()=>{
     header.classList.toggle("sticky",window.scrollY>100)
 })
+
+let menu=document.getElementById("menu-button")
+let navlist=document.querySelector(".navlist")
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x')
+    navlist.classList.toggle("open")
+}
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x')
+    navlist.classList.remove("open")
+}
